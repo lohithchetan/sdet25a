ff8a7329a2495982d126c5363e747d4231a599af
lohith <lohith@Lohith>
2021-12-10 09:38:00 +0530

    user-1 commit-1

---
+++ src/main/java/com/github/SDET/GitHub/App.java
@@ -0,0 +1,13 @@
+package com.github.SDET.GitHub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/github/SDET/GitHub/Hub.java
@@ -0,0 +1,14 @@
+package com.github.SDET.GitHub;
+
+import org.testng.annotations.Test;
+
+public class Hub {
+	
+	@Test
+	
+	public void storeData()
+	{
+		System.out.println("testing storage...");
+		System.out.println("test 1_");
+	}
+}
